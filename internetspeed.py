4145d5ef8e88f7eb8989b8694438548ccba3ed88
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-09 08:44:07 +0200

    styling my graphics

--- internetspeed.py
+++ internetspeed.py
@@ -5,4 +5,7 @@ sp = Tk()
 sp.title("Internet Speed Test") 
 sp.geometry("300x500")
 sp.config(bg = "Blue")
+
+lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab.place(x=40, y= 40)
 sp.mainloop()
