06fc4beb821594b72b320bccdd54ce4af08a2e5e
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-09 08:52:59 +0200

    added speed

--- internetspeed.py
+++ internetspeed.py
@@ -7,5 +7,22 @@ sp.geometry("300x500")
 sp.config(bg = "Blue")
 
 lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab.place(x=40, y= 40, height=30,)
+
+lab = Label(sp,text="00",bg="Blue",fg="White",font=("Arial",20,"bold"))
 lab.place(x=40, y= 40)
+
+lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab.place(x=40, y= 40)
+
+lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab.place(x=40, y= 40)
+
+lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab.place(x=40, y= 40)
+
+
+
+
+
 sp.mainloop()
