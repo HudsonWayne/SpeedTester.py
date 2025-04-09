8d6574867b5d53b79341547de355e307a3288b40
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-09 08:53:58 +0200

    added width

--- internetspeed.py
+++ internetspeed.py
@@ -7,7 +7,7 @@ sp.geometry("300x500")
 sp.config(bg = "Blue")
 
 lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
-lab.place(x=40, y= 40, height=30,)
+lab.place(x=40, y= 40, height=30,width=380,)
 
 lab = Label(sp,text="00",bg="Blue",fg="White",font=("Arial",20,"bold"))
 lab.place(x=40, y= 40)
