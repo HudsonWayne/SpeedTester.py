a827a5ba26d86c89f07f44ed24ff2c2983a42e2d
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-09 10:58:10 +0200

    initial commit

--- internetspeed.py
+++ internetspeed.py
@@ -12,7 +12,7 @@ lab.place(x=40, y= 40, height=30,width=380,)
 lab = Label(sp,text="00",bg="Blue",fg="White",font=("Arial",22,"bold"))
 lab.place(x=40, y= 40)
 
-lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
+lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",21,"bold"))
 lab.place(x=40, y= 40)
 
 lab = Label(sp,text="Internet Speed Test",bg="Blue",fg="White",font=("Arial",20,"bold"))
